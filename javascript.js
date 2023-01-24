204268a35611f2a31ed3217da646a3f51f510965
jgonzalezllado <jgonzalezllado@cifpfbmoll.eu>
2023-01-24 13:48:06 +0100

    ok

---
+++ javascript.js
@@ -0,0 +1,15 @@
+recipiente = "papel"
+//mostrar algo en pantalla, mostrar recipiente que veremos el papel
+alert(recipiente)
+//tipo de datos String Number, Boolean, Symbol
+//String
+string = "Cadena de texto"
+number = 19 //va sin comillas
+boolean = true  // puedes poner 0 o 1 o true o false
+//casos especiales de datos
+// declarar variable como var , let o const 
+var numero = 15;
+//actualmente usar let
+const nombre = "dalto" // la variable siempre será igual se suele usar para numero pi o algo que quieras modificar
+let numero; // declaramos variable
+numero = 29;  //es para inicializar la variable
